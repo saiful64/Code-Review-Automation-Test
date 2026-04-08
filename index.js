013123f5e9874824db201727b4bb9bb16d8250eb
Saiful <saifulislam778866@gmail.com>
2026-04-08 20:02:19 +0530

    test: bad code for copilot review

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function getData(a,b){
-console.log("start")
-if(a==b){
-return true
-}else{
-return false
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+function getData(a, b) {
+  console.log("start");
+  if (a == b) {
+    return true;
+  } else {
+    return false;
+  }
 }
-}
+
+app.listen(3000, () => console.log("Server running"));
